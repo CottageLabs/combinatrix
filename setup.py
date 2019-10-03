aa04de8e89876eec0a49ea08d7f47b0773efd9e2
Aga Domanska <aga@cottagelabs.com>
2019-10-03 13:11:58 +0100

    upgrade parametrized requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = '1.0.0',
     packages = find_packages(),
     install_requires = [
-        "parameterized==0.6.1"
+        "parameterized==0.7"
     ],
     url = 'http://cottagelabs.com/',
     author = 'Cottage Labs',
