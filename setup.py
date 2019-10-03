d8266af351c0352f039b08ed24bd9e6f4ffdbc4a
Steve Eardley <steve@eardley.xyz>
2019-10-03 22:04:16 +0100

    Fixes for setup.py, version change for python3

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-import sys
 from setuptools import setup, find_packages
 
 setup(
-    name = 'combinatrix',
-    version = '1.0.0',
-    packages = find_packages(),
-    install_requires = [
+    name='combinatrix',
+    version='2.0.0',
+    packages=find_packages(),
+    install_requires=[
         "parameterized==0.7"
     ],
-    url = 'http://cottagelabs.com/',
-    author = 'Cottage Labs',
-    author_email = 'us@cottagelabs.com',
-    description = 'Produce constrained combinations of parameters',
-    classifiers = [
+    url='http://cottagelabs.com/',
+    author='Cottage Labs',
+    author_email='us@cottagelabs.com',
+    description='Produce constrained combinations of parameters',
+    classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'combinatrix=combinatrix.cli:main',
         ],
